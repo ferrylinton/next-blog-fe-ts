38edf73266bcba0267efe1658d13255d07a9f011
Ferry L.H <ferrylinton@gmail.com>
2023-10-06 14:52:29 +0700

    update search form

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,16 +1,28 @@
+'use client';
+
 import CloseIcon from '@/icons/CloseIcon';
 import SearchIcon from '@/icons/SearchIcon';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
-import { ChangeEvent, useState } from 'react';
-
+import { ChangeEvent, FocusEvent, useRef, useState } from 'react';
 
 
 export default function SearchForm() {
 
+    const inputRef = useRef<HTMLInputElement>(null);
+
     const router = useRouter();
 
     const [keyword, setKeyword] = useState<string>(typeof router.query?.keyword === "string" ? router.query.keyword : '');
+
+
+    const handleFocus = (event: FocusEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+
+        if (inputRef.current && keyword.length === 0) {
+            inputRef.current.focus();
+        }
+    }
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
         setKeyword(event.target.value.replace(/\s/g, ''));
@@ -18,7 +30,13 @@ export default function SearchForm() {
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        event.currentTarget.submit();
+        
+        if (inputRef.current && keyword.length === 0) {
+            inputRef.current.focus();
+        } else {
+            event.currentTarget.submit();
+        }
+
     }
 
     const handleReset = () => {
@@ -36,25 +54,24 @@ export default function SearchForm() {
             onSubmit={handleSubmit}
             noValidate
             autoComplete='off'
-            className={`bg-white w-full sm:p-0 sm:w-[200px] md:w-[300px]`}>
-            <div className="relative flex w-full grow">
-                <button type="submit" className="absolute top-0 left-0 h-full w-[35px] text-stone-400 hover:text-stone-500">
-                    <SearchIcon className='w-[20px] h-[20px] mx-auto' />
-                </button>
-                <input type="text"
-                    name="keyword"
-                    onChange={handleChange}
-                    placeholder={t('keyword')}
-                    value={keyword}
-                    maxLength={20}
-                    className="w-full h-[36px] border border-stone-300 px-[35px] text-sm focus:outline-none focus:ring-2 ring-lime-200" />
-                {keyword && keyword.length > 0 &&
-                    <button type="button" onClick={() => handleReset()}
-                        className="absolute top-0 right-0 h-full w-[35px] text-stone-400 hover:text-stone-500">
-                        <CloseIcon className='w-[20px] h-[20px]  mx-auto' />
-                    </button>}
+            className='search-bar'>
 
-            </div>
+            <button type="submit" onFocus={handleFocus}>
+                <SearchIcon />
+            </button>
+            <input
+                ref={inputRef}
+                type="text"
+                name="keyword"
+                onChange={handleChange}
+                placeholder={t('keyword')}
+                value={keyword}
+                maxLength={20} />
+            {keyword && keyword.length > 0 &&
+                <button type="button" onClick={() => handleReset()}>
+                    <CloseIcon />
+                </button>}
+
         </form>
     )
 
